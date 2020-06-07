b9a063ed170bb03a6397cf14b72c2d2dfd150301
Filipsm94 <filipsm94@gmail.com>
2020-06-06 23:13:21 -0500

    change functional components to components based in class

--- src/app/components/persons/person/person.js
+++ src/app/components/persons/person/person.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './person.module.css';
 
-
-const Person = (props) => {
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>yo soy {props.name} y tengo {props.age}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    );
-
+export default class Person extends Component{
+    render(){
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>yo soy {this.props.name} y tengo {this.props.age}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    };
 }
-
-export default Person;
--- src/app/components/persons/persons.js
+++ src/app/components/persons/persons.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Component} from 'react';
 
 import Person from './person/person'
 
-
-const Persons = (props) => props.persons.map( (person, index) => {
-    return <Person
-                click={() => props.clicked(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event)=> props.changed(event, person.id)}
-
-     />
-});
-
-export default Persons;
+export default class Persons extends Component{
+    render(){
+        return this.props.persons.map( (person, index) => {
+            return <Person
+                        click={() => this.props.clicked(index)}
+                        name={person.name}
+                        age={person.age}
+                        key={person.id}
+                        changed={(event)=> this.props.changed(event, person.id)}
+        
+             />
+        });
+    }
+}
