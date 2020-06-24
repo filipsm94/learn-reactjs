c5868c20f8e136fec7390cd7ec17e310d5102f32
Filipsm94 <filipsm94@gmail.com>
2020-06-23 20:05:50 -0500

    add lib for show errores of typedata component

--- src/app/components/persons/person/person.js
+++ src/app/components/persons/person/person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './person.module.css';
 
 export default class Person extends Component{
@@ -10,5 +11,12 @@ export default class Person extends Component{
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
             </div>
         );
-    };
+    };   
+}
+
+Person.propTypes ={
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 }
